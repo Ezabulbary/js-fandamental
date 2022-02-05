79c0f1146ef789581213c59a4ddd2c7a5fc7ff8c
Ezabulbary <ezabul.bary@gmail.com>
2022-02-05 08:31:05 +0600

    js fandamental

---
+++ variable.js
@@ -0,0 +1,15 @@
+// number
+var price = 32;
+price = 12;
+
+
+// string 
+var myGf = "karina";
+
+
+// boolean 
+var isSunyDay = true;
+
+// integer : 1, 2, 3, 4, 5,
+
+// float: 1.12 1.14 2.67 4.65

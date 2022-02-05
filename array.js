b57bb62e103bddf7704e9df6610d8bec8b900a21
Ezabulbary <ezabul.bary@gmail.com>
2022-02-05 11:45:23 +0600

    array types

---
+++ array.js
@@ -0,0 +1,12 @@
+// many types of array
+var friendsAge = [11, 12, 13, 14, 15, 16, 17];
+
+var toure = [100, 2900, 1233, 1234, 123, 5678];
+
+var cricater = ["sakib", "mash", "mahamudUllah", "mehedi", "saifUddin"];
+
+var vowels = ["a", "e", "i", "o", "u"];
+
+var countCricater = cricater.length;
+
+console.log(countCricater);

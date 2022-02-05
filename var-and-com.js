feb1cd0bf0ea1f6dffe8e95355a9e8b16e1bfa9e
Ezabulbary <ezabul.bary@gmail.com>
2022-02-05 15:54:46 +0600

    variable and comparison oparetors

---
+++ var-and-com.js
@@ -0,0 +1,17 @@
+console.log(5 > 6);
+console.log(5 < 6);
+console.log(5 == 6);
+console.log(6 == 6);
+console.log(5 != 6);
+
+var myGf = 99;
+var yourGf = 100;
+console.log(myGf > yourGf);
+console.log(myGf < yourGf);
+console.log(myGf == yourGf);
+console.log(myGf != yourGf);
+
+// and oparetor
+console.log(banker && goodMan);
+// or oparetor
+console.log(banker || goodMan);

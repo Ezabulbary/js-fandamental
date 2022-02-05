849fb106f1cfe730547a51023b0c2cd1a1e658fe
Ezabulbary <ezabul.bary@gmail.com>
2022-02-05 16:21:24 +0600

    conditional js

---
+++ conditional.js
@@ -0,0 +1,33 @@
+var isFoodRady = true;
+
+if(isFoodRady = false){
+    console.log("than i eat now mama");
+}
+else{
+    console.log("than i watch video");
+}
+
+var chekenPice = 130;
+var eggPrice = 30;
+var food = chekenPice + eggPrice;
+var money = 500;
+
+if(food < money){
+    console.log("cheken diye ar egg diye muri makhabo");
+}
+else{
+    console.log("Taka niye bari cole jamu");
+}
+
+
+
+
+var chekenPice = 130;
+var money = 500;
+
+if (chekenPice < money){
+    console.log("cheken diye ar egg diye muri makhabo");
+}
+else{
+    console.log("Taka niye bari cole jamu");
+}

e41035be9f6cc1b46bc39afd62fea348577d6836
Ezabulbary <ezabul.bary@gmail.com>
2022-02-05 15:25:54 +0600

    push and pop

---
+++ push-pop.js
@@ -0,0 +1,17 @@
+// add an element to an array
+
+var lastBanch = ["hasan", "kawsar", "sorif", "korim"];
+lastBanch.push("mededi");
+lastBanch.push("shibly");
+console.log(lastBanch);
+
+var friendAge = [12, 16, 17, 42, 24, 23, 45];
+friendAge.push(32);
+console.log(friendAge);
+
+// remove an element from an array
+
+friendAge.pop();
+var lastItem = friendAge.pop();
+console.log(friendAge)
+console.log(lastItem);

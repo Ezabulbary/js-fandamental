3ed9768cee02def7770b5e0d89593837f9df9df3
Ezabulbary <ezabul.bary@gmail.com>
2022-02-05 14:39:25 +0600

    array index

--- array-index.js
+++ array-index.js
@@ -1,5 +1,30 @@
+// how to find element position 
+
+// string variable
+
 var books = ["one", "two", "three", "four", "five", "six", "seven"];
 
-var index = books.indexOf(three);
+var threeIndex = books.indexOf("two");
+var threeIndex = books.indexOf("ten");
 
-console.log(index);
+console.log(threeIndex);
+
+// number variable
+
+var numbers = [12, 23, 34, 45, 56, 78, 67];
+
+var fourthIndex = numbers.indexOf(12);
+var fourthIndex = numbers.indexOf(122);
+
+console.log(fourthIndex);
+
+// how to find element name
+var secondIndex = books[7];
+console.log(secondIndex);
+
+// how to change array element
+
+console.log(numbers);
+numbers [1] = 32;
+numbers [4] = 110;
+console.log(numbers);

97e946f9a8b74b7dfa6490e67a4115f2923844b2
Ezabulbary <ezabul.bary@gmail.com>
2022-02-05 17:47:09 +0600

    multiple condition in javascript

---
+++ multiple-condition.js
@@ -0,0 +1,10 @@
+var gotJob = false;
+var hasMoney = 200000;
+var hasHouse = true;
+
+if((gotJob == true && hasMoney > 100000) || hasHouse == true){
+    console.log("cholo biya kore feli");
+}
+else{
+    console.log("tor kopale biya nai")
+}
